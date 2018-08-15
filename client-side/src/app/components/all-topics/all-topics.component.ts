0503fb9fc9bedff9bc02f3ce6df816ddc2bed5c8
inh556 <yanbin.jin@gmail.com>
2018-08-15 12:59:31 -0700

    get dummy data from service

--- client-side/src/app/components/all-topics/all-topics.component.ts
+++ client-side/src/app/components/all-topics/all-topics.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TopicsService } from '../../services/topics.service';
+import { Topic } from '../../models/topic.model';
 @Component({
   selector: 'app-all-topics',
   templateUrl: './all-topics.component.html',
   styleUrls: ['./all-topics.component.css']
 })
 export class AllTopicsComponent implements OnInit {
-
-  constructor() { }
+  topics: Topic[];
+  constructor(private topicService: TopicsService) { }
 
   ngOnInit() {
+   this.topics =  this.topicService.getTopics();
   }
-
 }
