c3e66dc0af88612a825e94b208bf078a7c2a84b0
inh556 <yanbin.jin@gmail.com>
2018-08-14 18:52:39 -0700

    added routes

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routes'
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PopularTopicsComponent } from './components/popular-topics/popular-topics.component';
+import { AllTopicsComponent } from './components/all-topics/all-topics.component';
+import { TopicDetailComponent } from './components/topic-detail/topic-detail.component';
+import { NewTopicComponent } from './components/new-topic/new-topic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PopularTopicsComponent,
+    AllTopicsComponent,
+    TopicDetailComponent,
+    NewTopicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client-side/src/app/app.routes.ts
+++ client-side/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { PopularTapicsComponent } from './components/popular-tapics/popular-tapics.component';
+import { PopularTopicsComponent } from './components/popular-topics/popular-topics.component';
 import { AllTopicsComponent } from './components/all-topics/all-topics.component';
 import {NewTopicComponent} from './components/new-topic/new-topic.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: PopularTapicsComponent},
+  { path: 'home', component: PopularTopicsComponent},
   { path: 'topics', component: AllTopicsComponent},
   { path: 'newTopic', component: NewTopicComponent},
 ];
