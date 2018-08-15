2500f1ac6355d690814afe8033846fbbc04f9099
inh556 <yanbin.jin@gmail.com>
2018-08-14 18:50:32 -0700

    added new topic component

---
+++ client-side/src/app/components/new-topic/new-topic.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Topic } from '../../models/topic.model'
+const DEFAULT_TOPIC = {
+  id: 0,
+  title: '',
+  desc: '',
+  tag: 'life'
+}
+@Component({
+  selector: 'app-new-topic',
+  templateUrl: './new-topic.component.html',
+  styleUrls: ['./new-topic.component.css']
+})
+export class NewTopicComponent implements OnInit {
+  newTopic: Topic = Object.assign({}, DEFAULT_TOPIC);
+  tags: string[] = ['life', 'military', 'tv']
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
