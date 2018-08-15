81f8fc92973b412ef2cbceb3273eea8cecebd2ed
inh556 <yanbin.jin@gmail.com>
2018-08-15 12:59:52 -0700

    added dummy data

--- client-side/src/app/services/topics.service.ts
+++ client-side/src/app/services/topics.service.ts
@@ -1,12 +1,54 @@
 import { Injectable } from '@angular/core';
 
+const topics = [
+  {
+    id: 1,
+    title: "Test1",
+    desc: "test",
+    tag: "life",
+    likes: 1
+  },
+  {
+    id: 2,
+    title: "Test2",
+    desc: "test",
+    tag: "life",
+    likes: 3
+  },
+  {
+    id: 3,
+    title: "Test3",
+    desc: "test",
+    tag: "life",
+    likes: 3
+  },
+  {
+    id: 4,
+    title: "Test4",
+    desc: "test",
+    tag: "life",
+    likes: 5
+  },
+  {
+    id: 5,
+    title: "Test5",
+    desc: "test",
+    tag: "life",
+    likes: 9
+  },
+]
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class TopicsService {
 
   constructor() { }
   getTopics() {
+    return topics;
+  }
+  getPopularTopics() {
     
   }
 }
