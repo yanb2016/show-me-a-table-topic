3daa724a4d1ff4f191bd19fae4bfe5c6fb64f6e7
inh556 <yanbin.jin@gmail.com>
2018-08-21 21:10:56 -0700

    added server part

---
+++ server-side/auth/auth_checker.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const User = require('mongoose').model('User');
+const config = require('../config/config.json');
+
+module.exports = (req, res, next) => {
+  console.log('auth_checker: req: ' + req.headers);
+  if(!req.headers.authorization) {
+    return res.status(401).end();
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  console.log('auth_checker: token: ' + token);
+  return jwt.verify(token, config.jwtSecret, (err, decoded) => {
+    if(err) {return req.status(401).end()};
+    const id = decoded.sub;
+    return User.findById(id, (userErr, user) => {
+      if(userErr || !user) {
+        return res.status(401).end();
+      }
+      return next();
+    })
+  })
+}
