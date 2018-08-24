dd4a9d6e2e17336a0a9ad72289b81f27288f0be5
inh556 <yanbin.jin@gmail.com>
2018-08-23 21:00:23 -0700

    imported httpModule

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -8,7 +8,10 @@ import { PopularTopicsComponent } from './components/popular-topics/popular-topi
 import { AllTopicsComponent } from './components/all-topics/all-topics.component';
 import { TopicDetailComponent } from './components/topic-detail/topic-detail.component';
 import { NewTopicComponent } from './components/new-topic/new-topic.component';
-
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,12 +19,17 @@ import { NewTopicComponent } from './components/new-topic/new-topic.component';
     PopularTopicsComponent,
     AllTopicsComponent,
     TopicDetailComponent,
-    NewTopicComponent
+    NewTopicComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     routing,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
