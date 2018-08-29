daf721e9f47e1503ff3162f813cd9dffa941b627
inh556 <yanbin.jin@gmail.com>
2018-08-29 16:23:10 -0700

    added observable

--- client-side/src/app/services/auth.service.ts
+++ client-side/src/app/services/auth.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
   constructor() { }
   authenticateUser(token, email) {
     localStorage.setItem('token', token);
     localStorage.setItem('email', email);
   }
-  isUserAuthenticated() {
-    return localStorage.getItem('token') !== null;
+  isUserAuthenticated(): Observable<boolean> {
+    return of(localStorage.getItem('token') !== null)
   }
+
   deauthenticateUser() {
     localStorage.removeItem('token');
     localStorage.removeItem('email');
