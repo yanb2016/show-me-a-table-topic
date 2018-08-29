3a4cf16d927159a2f2b4ea4fe3f184e855595e08
inh556 <yanbin.jin@gmail.com>
2018-08-28 17:14:56 -0700

    cleanup topicdetail

--- client-side/src/app/components/topic-detail/topic-detail.component.ts
+++ client-side/src/app/components/topic-detail/topic-detail.component.ts
@@ -1,27 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Topic } from '../../models/topic.model';
-//import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../services/data.service';
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-topic-detail',
   templateUrl: './topic-detail.component.html',
   styleUrls: ['./topic-detail.component.css']
 })
 export class TopicDetailComponent implements OnInit {
-  problem: Topic;
+  author: string;
+  topic: Topic;
   constructor(private dataService: DataService, 
               private route: ActivatedRoute,  
-  //            private location: Location,
+              private auth: AuthService,
               ) { }
 
   ngOnInit() {
-    // this.route.params.subscribe(params => {
-    //   this.problem = this.dataService.getProblem(+params['id'])
-    // })
     this.route.params.subscribe(params => {
-      this.dataService.getProblem(+params['id'])
-      .then(problem => this.problem = problem);
+      this.dataService.getTopic(+params['id'])
+      .then(topic => this.topic = topic)
     })
+    this.author = this.auth.getEmail();
   }
 }
