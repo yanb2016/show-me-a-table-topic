a1943021040d19120ad4733bec82f00f649847a1
inh556 <yanbin.jin@gmail.com>
2018-08-28 17:10:31 -0700

    added routelink to signup

--- client-side/src/app/components/login/login.component.ts
+++ client-side/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,52 +13,43 @@ export class LoginComponent implements OnInit {
     email: '',
     password: ''
   }
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router
+             ) { }
 
   ngOnInit() {
   }
-  // getUser() {
-  //   console.log('done!')
-  //   console.log(this.user);
-  // }
-  // get diagnostic() { return this.user.email; }
   checkUser() {
-    //event.preventDefault();
     const email = this.user.email;
     const password = this.user.password;
-    // need to pass to server later
-    console.log(email);
-    console.log(password);
-
     const url = 'http://' + window.location.hostname + ':3001/auth/login';
     const request = new Request(
       url,
       {
-        method: 'POST', 
+        method: 'POST',
         headers: {
           'Accept': 'application/json',
-          'Content.Type': 'application/json'
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          email: this.user.email,
-          password: this.user.password
+          email: email,
+          password: password
         })
-    });
+      }
+    )
     fetch(request).then(response => {
-      console.log(response.json());
-      if(response.status === 200) {
+      if (response.status === 200) {
         this.errors = {};
         response.json().then(json => {
-          console.log(json);
           this.auth.authenticateUser(json.token, email);
-          window.location.replace('/');
-        })
-      } 
-      else {
+        });
+        this.router.navigateByUrl('/'); 
+      } else {
         console.log('Login failed');
         response.json().then(json => {
-          const errors = json.errors ? json.error: {};
-          errors['summary'] = json.message;
+          console.log(json);
+          this.errors = json.errors ? json.error: {};
+          this.errors['summary'] = json.message;
         })
       }
     } 
