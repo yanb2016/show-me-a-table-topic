86255e6f63b294f83b8e9fe465df2541706b8016
Yanbin Jin <yanbinjin@Yanbins-MacBook-Pro.local>
2018-09-04 17:05:06 -0700

    added route to searchResult componet

--- client-side/src/app/app.routes.ts
+++ client-side/src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { TopicEditorComponent } from './components/topic-editor/topic-editor.component'
 import { MostPopularTopicComponent } from './components//most-popular-topic/most-popular-topic.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: MostPopularTopicComponent },
   { path: 'topics', component:TopicListComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'topics/:category', component: SearchResultsComponent },
   { path: 'topic/:id', component: TopicDetailComponent },
   { path: 'topic_ditor/:id', component: TopicEditorComponent },
   { path: 'new_topic', component: NewTopicComponent },
