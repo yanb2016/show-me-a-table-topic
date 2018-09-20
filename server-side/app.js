4ef1b8cdea242138efff00f907c3150ffdbb96ca
inh556 <yanbin.jin@gmail.com>
2018-09-19 20:16:12 -0700

    change port to ready to deploy to aws

--- server-side/app.js
+++ server-side/app.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var config = require('./config/config')
 require('./models/main').connect(config.mongoDbUri, { useNewUrlParser: true });
-var port = process.env.PORT || 3003 ;
+var port = process.env.PORT || 3000 ;
 var path = require('path');
 var passport = require('passport');
 var restRouter = require('./routes/rest');
@@ -18,7 +18,7 @@ app.use(passport.initialize());
 passport.use('local-signup', require('./auth/signup_local_strategy'));
 passport.use('local-login', require('./auth/login_local_strategy'));
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(morgan('dev'));
 
 app.use('/auth', auth);
@@ -26,7 +26,7 @@ app.use('/api/v1', restRouter);
 // app.use('/editor', authChecker);
 app.use('/editor', editor);
 app.use((req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../public')});
+  res.sendFile('index.html', { root: path.join(__dirname, './public')});
   })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
