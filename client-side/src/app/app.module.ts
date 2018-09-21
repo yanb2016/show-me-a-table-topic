591f21629e1e3f005acf7deb4cd88758f0af8d0f
inh556 <yanbin.jin@gmail.com>
2018-09-20 20:50:37 -0700

    added about component and route to about

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,31 +3,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopicListComponent } from './components/topic-list/topic-list.component';
-import { TopicDetailComponent } from './components/topic-detail/topic-detail.component';
 import { routing } from './app.routes';
 import { NewTopicComponent } from './components/new-topic/new-topic.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchPipe } from './pipes/search.pipe';
-import { TopicEditorComponent } from './components/topic-editor/topic-editor.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { MostPopularTopicComponent } from './components/most-popular-topic/most-popular-topic.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { IdeasComponent } from './components/ideas/ideas.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TopicDetailComponent } from './components/topic-detail/topic-detail.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopicListComponent,
-    TopicDetailComponent,
     NewTopicComponent,
     NavBarComponent,
     SearchPipe,
-    TopicEditorComponent,
     SignupComponent,
     LoginComponent,
     MostPopularTopicComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
+    IdeasComponent,
+    FooterComponent,
+    TopicDetailComponent,
+    AboutComponent
 
   ],
   imports: [
