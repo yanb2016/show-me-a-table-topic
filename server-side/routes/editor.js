58df093bd9fcedc50b057db7c6d64ecadbe6e3c0
inh556 <yanbin.jin@gmail.com>
2018-09-20 20:59:28 -0700

    add error info

--- server-side/routes/editor.js
+++ server-side/routes/editor.js
@@ -10,12 +10,20 @@ router.post('/topics', jsonParser, (req, res) => {
     .then(message => {
       res.json(message)
     }, (error) => {
-      res.status(400).send("Not saved! Topic already existing!")
+      res.status(400).send({
+        "info": "Topic already existing!",
+        "instruction": `Aww yeah, you successfully read this important alert message.
+        It seems someone else has already submitted this topic. You can find it via searching or you can click
+        the link below to edit your own topic.`,
+        "link": "Click here to to eidt"
+      })
     })
 })
 
-router.put('/topics', jsonParser, (req, res) => {
-  topicService.modifyTopic(req.body)
+router.put('/topics/:id/:action', jsonParser, (req, res) => {
+  id = req.params.id;
+  action = req.params.action;
+  topicService.handleLikes(id, action)
     .then(topic => {
       res.json(topic)
     }, (error) => {
