1af9fb87ab8d7f1435ddbbb1c3b5c7b98a6f6fbc
inh556 <yanbin.jin@gmail.com>
2018-09-20 21:00:19 -0700

    added catch to handle errs

--- server-side/routes/rest.js
+++ server-side/routes/rest.js
@@ -6,7 +6,6 @@ router.get('/topics/pagenumber/:pageNumber', function(req, res) {
   const pageNumber = req.params.pageNumber;
   topicService.getTopics(+pageNumber)
     .then(topics => res.json(topics), (error) => {
-      console.log(error)
 			res.status(400).send(error);
 		})
 });
@@ -14,18 +13,21 @@ router.get('/popularTopics', function(req, res) {
   topicService.getPopularTopic()
     .then(topics => res.json(topics));
 });
-router.get('/ideas', (req,res) => {
-  topicService.getIdeas()
-  .then(ideas => res.json(ideas))
-})
-router.get('/topic/:id', (req, res) => {
-  const id = req.params.id;
-  topicService.getTopic(+id)
-    .then(topic => res.json(topic))
+router.get('/ideas/pagenumber/:pageNumber', (req,res) => {
+  const pageNumber = req.params.pageNumber;
+  topicService.getIdeas(pageNumber)
+  .then(ideas => res.json(ideas), (error) => {
+    console.log(error)
+    res.status(400).send(error);
+  })
 });
-router.get('/topics/:category', (req, res) => {
+router.get('/topics/:category/:pageNumber', (req, res) => {
   const category = req.params.category;
-  topicService.getSearchedTopics(category)
-    .then(topics => res.json(topics))
+  const pageNumber= req.params.pageNumber;
+  topicService.getSearchedTopics(category, pageNumber)
+    .then(topics => res.json(topics), (error) => {
+      console.log(error)
+      res.status(400).send(error);
+    })
 });
 module.exports = router;
